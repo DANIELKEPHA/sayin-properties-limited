d2e5e8fdcdb1806682b7004cb8d28b33f87f1a11
Daniel Kepha <danielkepha199@gmail.com>
2025-01-20 22:06:24 +0300

    add redux toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import {
@@ -7,15 +7,17 @@ import {
   signInFailure,
 } from "../redux/user/userSlice";
 
-export default function Signin() {
+export default function SignIn() {
   const [formData, setFormData] = useState({});
   const { loading, error } = useSelector((state) => state.user);
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.id]: e.target.value });
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
   };
-
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
@@ -28,15 +30,15 @@ export default function Signin() {
         body: JSON.stringify(formData),
       });
       const data = await res.json();
+      console.log(data);
       if (data.success === false) {
         dispatch(signInFailure(data.message));
         return;
       }
-      dispatch(signInSuccess(data.message));
+      dispatch(signInSuccess(data));
       navigate("/");
     } catch (error) {
       dispatch(signInFailure(error.message));
-      setError(error.message);
     }
   };
   return (
@@ -44,22 +46,23 @@ export default function Signin() {
       <h1 className="text-3xl text-center font-semibold my-7">Sign In</h1>
       <form onSubmit={handleSubmit} className="flex flex-col gap-4">
         <input
-          className="border p-3 rounded-lg"
           type="email"
           placeholder="email"
+          className="border p-3 rounded-lg"
           id="email"
           onChange={handleChange}
         />
         <input
-          className="border p-3 rounded-lg"
           type="password"
           placeholder="password"
+          className="border p-3 rounded-lg"
           id="password"
           onChange={handleChange}
         />
+
         <button
           disabled={loading}
-          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-90 disabled:opacity-70"
+          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
         >
           {loading ? "Loading..." : "Sign In"}
         </button>
@@ -67,10 +70,10 @@ export default function Signin() {
       <div className="flex gap-2 mt-5">
         <p>Dont have an account?</p>
         <Link to={"/sign-up"}>
-          <span className="text-blue-700">Sign Up</span>
+          <span className="text-blue-700">Sign up</span>
         </Link>
       </div>
-      {error && <div className="text-red-700">{error}</div>}
+      {error && <p className="text-red-500 mt-5">{error}</p>}
     </div>
   );
 }
