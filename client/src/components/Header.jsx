ccda2e640e6bc832b787b323d143caefef1d32f2
Daniel Kepha <danielkepha199@gmail.com>
2025-01-20 15:27:50 +0300

    complete sign up functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -26,31 +26,17 @@ export default function Header() {
         </form>
         <ul className="flex gap-4">
           <Link to="/">
-            <li>
-              <a
-                href="#"
-                className="hidden sm:inline text-slate-700 hover:underline"
-              >
-                Home
-              </a>
+            <li className="hidden sm:inline text-slate-700 hover:underline">
+              Home
             </li>
           </Link>
           <Link to="/about">
-            <li>
-              <a
-                href="#"
-                className="hidden sm:inline text-slate-700 hover:underline"
-              >
-                About
-              </a>
+            <li className="hidden sm:inline text-slate-700 hover:underline">
+              About
             </li>
           </Link>
           <Link to="sign-in">
-            <li>
-              <a href="#" className="text-slate-700 hover:underline">
-                Sign In
-              </a>
-            </li>
+            <li className="text-slate-700 hover:underline">Sign In</li>
           </Link>
         </ul>
       </div>
