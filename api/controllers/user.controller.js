b7f927bb5f03afa28626936344032e29091129ca
Daniel Kepha <danielkepha199@gmail.com>
2025-01-22 11:05:13 +0300

    user updated api

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,3 +1,44 @@
+import bcryptjs from "bcryptjs";
+import User from "../models/user.model.js";
+import { errorHandler } from "../utils/error.js";
+// import Listing from "../models/listing.model.js";
+
 export const test = (req, res) => {
-  res.status(200).json({ message: "Hello, World!" });
+  res.json({
+    message: "Api route is working!",
+  });
+};
+
+export const updateUser = async (req, res, next) => {
+  try {
+    if (req.user.id !== req.params.id) {
+      return next(errorHandler(401, "You can only update your own account!"));
+    }
+
+    const updateFields = {
+      username: req.body.username,
+      email: req.body.email,
+      avatar: req.body.avatar,
+    };
+
+    if (req.body.password) {
+      updateFields.password = bcryptjs.hashSync(req.body.password, 10);
+    }
+
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      { $set: updateFields },
+      { new: true }
+    );
+
+    if (!updatedUser) {
+      return next(errorHandler(404, "User not found!"));
+    }
+
+    const { password, ...rest } = updatedUser._doc;
+
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
 };
