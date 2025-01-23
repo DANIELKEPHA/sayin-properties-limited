026dd07dc9b684496ad7c9b1714bc92932b25b37
Daniel Kepha <danielkepha199@gmail.com>
2025-01-23 10:47:16 +0300

    complete upload listing images funtionality

---
+++ client/src/pages/CreateListing.jsx
@@ -0,0 +1,254 @@
+import { useState } from "react";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import { app } from "../firebase";
+
+export default function CreateListing() {
+  const [files, setFiles] = useState([]);
+  const [formData, setFormData] = useState({
+    imageUrls: [],
+  });
+  const [imageUploadError, setImageUploadError] = useState(false);
+  const [uploading, setUploading] = useState(false);
+
+  console.log(formData);
+
+  const handleFileChange = (e) => {
+    const maxFileSize = 2 * 1024 * 1024; // 2 MB
+    const selectedFiles = Array.from(e.target.files);
+    const oversizedFiles = selectedFiles.filter(
+      (file) => file.size > maxFileSize
+    );
+
+    if (oversizedFiles.length > 0) {
+      setImageUploadError("Each image must be less than 2 MB.");
+      setFiles([]);
+    } else {
+      setImageUploadError(false);
+      setFiles(selectedFiles);
+    }
+  };
+
+  const handleImageSubmit = () => {
+    if (files.length > 0 && files.length + formData.imageUrls.length < 7) {
+      setUploading(true);
+      setImageUploadError(false);
+      const promises = [];
+
+      for (let i = 0; i < files.length; i++) {
+        promises.push(storeImage(files[i]));
+      }
+      Promise.all(promises)
+        .then((urls) => {
+          setFormData({
+            ...formData,
+            imageUrls: formData.imageUrls.concat(urls),
+          });
+          setImageUploadError(false);
+          setUploading(false);
+        })
+        .catch(() => {
+          setImageUploadError("Image upload failed (2 MB max per image).");
+          setUploading(false);
+        });
+    } else {
+      setImageUploadError("You can only upload 6 images per listing.");
+      setUploading(false);
+    }
+  };
+
+  const storeImage = async (file) => {
+    return new Promise((resolve, reject) => {
+      const storage = getStorage(app);
+      const fileName = new Date().getTime() + file.name;
+      const storageRef = ref(storage, fileName);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log(`Upload is ${progress}% done`);
+        },
+        (error) => {
+          reject(error);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            resolve(downloadURL);
+          });
+        }
+      );
+    });
+  };
+  const handleRemoveImage = (index) => {
+    setFormData({
+      ...formData,
+      imageUrls: formData.imageUrls.filter((_, i) => i !== index),
+    });
+  };
+
+  return (
+    <div>
+      <main className="p-3 max-w-4xl mx-auto">
+        <h1 className="text-3xl font-semibold text-center my-7">
+          Create Listing
+        </h1>
+        <form className="flex flex-col sm:flex-row gap-4">
+          <div className="flex flex-col gap-4 flex-1">
+            <input
+              className="border p-3 rounded-lg"
+              type="text"
+              id="title"
+              placeholder="Name"
+              maxLength="62"
+              minLength="10"
+              required
+            />
+            <textarea
+              className="border p-3 rounded-lg"
+              type="text"
+              id="description"
+              placeholder="Description"
+              required
+            />
+            <input
+              className="border p-3 rounded-lg"
+              type="text"
+              id="address"
+              placeholder="Address"
+              required
+            />
+            <div className="flex gap-6 flex-wrap">
+              <div className="flex gap-2">
+                <input type="checkbox" id="sell" className="w-5" />
+                <span>Sell</span>
+              </div>
+              <div className="flex gap-2">
+                <input type="checkbox" id="rent" className="w-5" />
+                <span>Rent</span>
+              </div>
+              <div className="flex gap-2">
+                <input type="checkbox" id="parking" className="w-5" />
+                <span>Parking Spot</span>
+              </div>
+              <div className="flex gap-2">
+                <input type="checkbox" id="furnished" className="w-5" />
+                <span>Furnished</span>
+              </div>
+              <div className="flex gap-2">
+                <input type="checkbox" id="offer" className="w-5" />
+                <span>Offer</span>
+              </div>
+            </div>
+            <div className="flex flex-wrap gap-6 ">
+              <div className="flex items-center gap-2">
+                <input
+                  className="p-3 border border-gray-300 rounded-lg"
+                  type="number"
+                  id="bedroom"
+                  min="1"
+                  max="10"
+                  required
+                />
+                Beds
+              </div>
+              <div className="flex items-center gap-2">
+                <input
+                  className="p-3 border border-gray-300 rounded-lg"
+                  type="number"
+                  id="bathroom"
+                  min="1"
+                  max="10"
+                  required
+                />
+                Bathrooms
+              </div>
+              <div className="flex items-center gap-2">
+                <input
+                  className="p-3 border border-gray-300 rounded-lg"
+                  type="number"
+                  id="regularPrice"
+                  min="1"
+                  required
+                />
+                <div className="flex flex-col items-center">
+                  <p>Regular Price</p>
+                  <span className="text-xm">(Ksh / month)</span>
+                </div>
+              </div>
+              <div className="flex items-center gap-2">
+                <input
+                  className="p-3 border border-gray-300 rounded-lg"
+                  type="number"
+                  id="discountedPrice"
+                  min="1"
+                  required
+                />
+                <div className="flex flex-col items-center">
+                  <p>Discounted Price</p>
+                  <span className="text-xm">(Ksh / month)</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col gap-4 flex-1">
+            <p className="font-semibold">
+              Images:
+              <span className="font-normal text-gray-600 ml-2"></span>The first
+              image will be the cover (max 6)
+            </p>
+            <div className="flex gap-4">
+              <input
+                onChange={handleFileChange}
+                className="p-3 border border-gray-300 rounded w-full"
+                type="file"
+                id="images"
+                accept="image/*"
+                multiple
+              />
+              <button
+                type="button"
+                onClick={handleImageSubmit}
+                className="p-3 text-green-700 border border-green-700 uppercase hover:shadow-lg disabled:opacity-80"
+                disabled={uploading}
+              >
+                {uploading ? "Uploading..." : "Upload"}
+              </button>
+            </div>
+            <p className="text-red-700 text-sm">
+              {imageUploadError && imageUploadError}
+            </p>
+            {formData.imageUrls.length > 0 &&
+              formData.imageUrls.map((url, index) => (
+                <div
+                  key={url}
+                  className="flex justify-between p-3 border items-center"
+                >
+                  <img
+                    src={url}
+                    alt="listing image"
+                    className="w-20 h-20 object-contain rounded-lg"
+                  />
+                  <button
+                    type="button"
+                    onClick={() => handleRemoveImage(index)}
+                    className="p-3 text-red-700 rounded-lg uppercase hover:opacity-75"
+                  >
+                    Delete
+                  </button>
+                </div>
+              ))}
+            <button className="p-3 bg-slate-700 text-white rounded-lg uppercase">
+              Create Listing
+            </button>
+          </div>
+        </form>
+      </main>
+    </div>
+  );
+}
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -182,6 +182,12 @@ export default function Profile() {
         >
           {loading ? "Loading..." : "Update"}
         </button>
+        <Link
+          className="bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95"
+          to={"/create-listing"}
+        >
+          Create listing
+        </Link>
       </form>
       <div className="flex justify-between mt-5">
         <span
